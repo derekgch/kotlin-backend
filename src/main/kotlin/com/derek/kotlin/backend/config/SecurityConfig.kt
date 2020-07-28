45b42b33745915ec02dc8fa1ff0810c12899f489
Derek G Chen <derek@derekgc.com>
2020-07-28 19:14:45 -0400

    added an sample user and secure a few routes

--- src/main/kotlin/com/derek/kotlin/backend/config/SecurityConfig.kt
+++ src/main/kotlin/com/derek/kotlin/backend/config/SecurityConfig.kt
@@ -3,6 +3,7 @@ package com.derek.kotlin.backend
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
@@ -17,22 +18,28 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
         return BCryptPasswordEncoder()
     }
 
+
     override fun configure(auth: AuthenticationManagerBuilder) {
         auth.inMemoryAuthentication()
                 .withUser("test1")
                 .password(encoder().encode("pass1234"))
-                .roles("manager")
+                .roles("ADMIN")
+                .and()
+                .withUser("user1")
+                .password(encoder().encode("user1234"))
+                .roles("USER")
     }
 
-    // @Throws(Exception::class)
-    // override fun configure(http: HttpSecurity) {
-    //     http.httpBasic()
-    //             .and()
-    //             .authorizeRequests()
-    //             .antMatchers(HttpMethod.GET, "/*").hasRole("manager")
-    //             .and()
-    //             .csrf().disable()
-    //             .formLogin().disable()
-    // }
+     @Throws(Exception::class)
+     override fun configure(http: HttpSecurity) {
+         http.httpBasic()
+                 .and()
+                 .authorizeRequests()
+                 .antMatchers(HttpMethod.GET, "/").permitAll()
+                 .antMatchers(HttpMethod.GET, "/api/**").hasRole("ADMIN")
+                 .and()
+                 .csrf().disable()
+                 .formLogin().disable()
+     }
 
 }
