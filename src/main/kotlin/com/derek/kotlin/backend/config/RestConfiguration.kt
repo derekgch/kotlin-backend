cb68fd0acf20f7d504bca21fdad2f454bcae9163
Derek G Chen <derek@derekgc.com>
2020-07-28 19:16:01 -0400

    remove unused import

--- src/main/kotlin/com/derek/kotlin/backend/config/RestConfiguration.kt
+++ src/main/kotlin/com/derek/kotlin/backend/config/RestConfiguration.kt
@@ -1,5 +1,6 @@
-package com.derek.kotlin.backend
+package com.derek.kotlin.backend.config
 
+import com.derek.kotlin.backend.BookStoreModel
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer  
@@ -7,7 +8,7 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer
 @Configuration  
 open class RestConfiguration : RepositoryRestConfigurer {  
   override fun configureRepositoryRestConfiguration(config: RepositoryRestConfiguration?) {  
-    config?.exposeIdsFor(BookStoreModel::class.java) 
+    config?.exposeIdsFor(BookStoreModel::class.java)
     config?.setBasePath("/api"); 
   }  
 }
