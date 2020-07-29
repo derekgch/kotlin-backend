694097ca5e77aca0de7f3b4eb31124708188388f
Derek G Chen <derek@derekgc.com>
2020-07-29 18:32:50 -0400

    added bouncy castle dep and using argon2 crypt

--- src/main/kotlin/com/derek/kotlin/backend/config/SecurityConfig.kt
+++ src/main/kotlin/com/derek/kotlin/backend/config/SecurityConfig.kt
@@ -6,7 +6,7 @@ import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
@@ -14,7 +14,7 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     @Bean
     fun encoder(): PasswordEncoder {
-        return BCryptPasswordEncoder()
+        return Argon2PasswordEncoder()
     }
 
 
